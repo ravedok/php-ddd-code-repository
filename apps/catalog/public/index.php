fffd3415e2fe58e0c110b23d2c050a87ddb8d192
Jose Ramon Macia Gaitan <joseramon.macia@atlasinformatica.com>
2022-08-30 22:10:41 +0000

    Shared Vendor

--- apps/catalog/public/index.php
+++ apps/catalog/public/index.php
@@ -2,7 +2,10 @@
 
 use Ravedok\Apps\Catalog\Kernel;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+$_SERVER['APP_RUNTIME_OPTIONS']['dotenv_path'] = 'apps/catalog/.env';
+
+
+require_once dirname(__DIR__).'/../../vendor/autoload_runtime.php';
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
