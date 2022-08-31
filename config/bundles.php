c0b1a1389d569730743b3858964786b01c9cf4a6
Jose Ramon Macia Gaitan <joseramon.macia@atlasinformatica.com>
2022-08-31 18:39:56 +0000

    Add Persistence

--- config/bundles.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-];
--- src/Catalog/Products/Infrastructure/DoctrineProductRepository.php
+++ src/Catalog/Products/Infrastructure/DoctrineProductRepository.php
@@ -4,12 +4,23 @@ declare(strict_types=1);
 
 namespace Ravedok\Catalog\Products\Infrastructure;
 
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Ravedok\Catalog\Products\Domain\Product;
 use Ravedok\Catalog\Products\Domain\ProductRepository;
 
-final class DoctrineProductRepository implements ProductRepository
+/**
+ * @extends ServiceEntityRepository<Product>
+ */
+final class DoctrineProductRepository extends ServiceEntityRepository implements ProductRepository
 {
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Product::class);
+    }
+
     public function save(Product $product): void
     {
+        $this->getEntityManager()->persist($product);
     }
 }
